71c3135c171186dfd4ac5abb2f6ba605592aec0e
Souheil CHELFOUH <trollfot@vittra.org>
2009-10-20 21:01:23 +0200

    Moved to 0.3dev after tagging

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os.path import join
 
 name = 'dolmen.content'
-version = '0.2.1'
+version = '0.3dev'
 readme = open(join('src', 'dolmen', 'content', 'README.txt')).read()
 history = open(join('docs', 'HISTORY.txt')).read()
 
@@ -40,7 +40,7 @@ setup(name = name,
       namespace_packages = ['dolmen'],
       include_package_data = True,
       platforms = 'Any',
-      zip_safe = True,
+      zip_safe = False,
       tests_require = tests_require,
       install_requires = install_requires,
       extras_require = {'test': tests_require},
