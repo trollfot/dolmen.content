d8da9e30d453deedc6fc2146cecf847e9cd1d09e
Souheil CHELFOUH <trollfot@gmail.com>
2010-07-13 17:29:21 +0200

    Updated setup.py version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os.path import join
 
 name = 'dolmen.content'
-version = '0.4'
+version = '0.5'
 readme = open(join('src', 'dolmen', 'content', 'README.txt')).read()
 history = open(join('docs', 'HISTORY.txt')).read()
 
