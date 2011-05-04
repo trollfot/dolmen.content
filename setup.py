08d63ce698f2c22e05d8c62cd71081aaf4ecf051
Souheil CHELFOUH <trollfot@gmail.com>
2011-05-04 16:18:56 +0200

    Added License and credits.

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
+# -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
 from os.path import join
 
 name = 'dolmen.content'
 version = '2.0a1'
-readme = (open(join('src', 'dolmen', 'content', 'README.txt')).read() + '\n' +
-          open(join('src', 'dolmen', 'content', 'test_main.txt')).read())
+readme = open(join('src', 'dolmen', 'content', 'README.txt')).read()
 history = open(join('docs', 'HISTORY.txt')).read()
 
 install_requires = [
     'ZODB3 >= 3.10',  # Persistent.
     'dolmen.container',
     'dolmen.field >= 0.3',
-    'grokcore.component >= 2.2',
+    'grokcore.component >= 2.4',
     'grokcore.security',
-    'martian >= 0.12',
+    'martian >= 0.14',
     'setuptools',
     'zope.component',
     'zope.i18nmessageid',
@@ -26,30 +27,27 @@ tests_require = [
     'zope.testing',
     ]
 
-setup(name = name,
-      version = version,
-      description = 'Dolmen content type framework',
-      long_description = readme + '\n\n' + history,
-      keywords = 'Grok Zope3 CMS Dolmen Content',
-      author = 'Souheil Chelfouh',
-      author_email = 'souheil@chelfouh.com',
-      url = 'http://gitweb.dolmen-project.org/',
-      download_url = '',
-      license = 'GPL',
+setup(name=name,
+      version=version,
+      description='Dolmen content type framework',
+      long_description=readme + '\n\n' + history,
+      keywords='Grok Dolmen Content',
+      author='The Dolmen team',
+      author_email='dolmen@list.dolmen-project.org',
+      url='http://gitweb.dolmen-project.org/',
+      download_url='',
+      license='ZPL',
       packages=find_packages('src', exclude=['ez_setup']),
       package_dir={'': 'src'},
-      namespace_packages = ['dolmen'],
-      include_package_data = True,
-      platforms = 'Any',
-      zip_safe = False,
-      tests_require = tests_require,
-      install_requires = install_requires,
-      extras_require = {'test': tests_require},
-      classifiers = [
+      namespace_packages=['dolmen'],
+      include_package_data=True,
+      platforms='Any',
+      zip_safe=False,
+      tests_require=tests_require,
+      install_requires=install_requires,
+      extras_require={'test': tests_require},
+      classifiers=[
         'Environment :: Web Environment',
-        'Framework :: Zope3',
-        'Intended Audience :: Other Audience',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
       ],
--- src/dolmen/content/README.txt
+++ src/dolmen/content/README.txt
@@ -3,15 +3,6 @@ dolmen.content
 ==============
 
 The package `dolmen.content` is a convenient way to define content
-types.
-
-.. contents::
-
-About
-=====
-
-Content types usually have several attributes defined in a
-schema and a name. In addition, they often need security to control the
-creation or the modification (update, deletion, etc.). This is what
-provides `dolmen.content`, with an easy-to-use set of grok directives.
-
+types, using the zope component architecture (ZCA). It provides a set
+of Grok directives and base components, including a simple content and a 
+container.
